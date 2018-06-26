7ca7cbc27a61a33e81df5d36773ff2bd950bc8b5
Alex Hagen <alexhagen6@gmail.com>
2018-06-26 08:37:25 -0700

    initial pseudocode

---
+++ profiler.py
@@ -0,0 +1,8 @@
+from IPython.display import display, Javascript
+
+def profile():
+    # save current notebook
+    # open the notebook
+    # go through and check "TODO"s
+    # go through and check "REQUIREMENT"s
+    # go through and check "ERROR CHECK"s
